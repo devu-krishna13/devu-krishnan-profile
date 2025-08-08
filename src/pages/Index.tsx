e2250b99fafcb2e923302a0fb72050e2532e9f34
Devu Krishnan P <devukrishnanp@gmail.com>
2025-08-08 20:18:30 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,6 +4,7 @@ import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
+import Portfolio from "@/components/Portfolio";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
@@ -19,6 +20,7 @@ const Index = () => {
         <Skills />
         <Experience />
         <Projects />
+        <Portfolio />
         <Contact />
       </main>
       <Footer />
